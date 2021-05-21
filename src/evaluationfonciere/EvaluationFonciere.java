d5147a3fb27251e3d72cc3f8bfaa56a6e9967ae6
alex laurin <alexlaurincoding@gmail.com>
2021-05-21 19:32:06 -0400

    branche alex

--- src/evaluationfonciere/EvaluationFonciere.java
+++ src/evaluationfonciere/EvaluationFonciere.java
@@ -16,6 +16,7 @@ public class EvaluationFonciere {
      */
     public static void main(String[] args) {
         System.out.println("Sprint 1");
+        System.out.println("Alex branch");
     }
     
 }
