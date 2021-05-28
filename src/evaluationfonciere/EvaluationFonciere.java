9a6b9194392fa3c31e1e14dfc236581f50d73a5d
alex laurin <alexlaurincoding@gmail.com>
2021-05-28 00:37:33 -0400

    lecture du fichier json passer en parametre et creation d'un fichier sortie nomme par le deuxieme parametre

--- src/evaluationfonciere/EvaluationFonciere.java
+++ src/evaluationfonciere/EvaluationFonciere.java
@@ -5,6 +5,10 @@
  */
 package evaluationfonciere;
 
+import java.io.IOException;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 /**
  *
  * @author Alex
@@ -14,9 +18,22 @@ public class EvaluationFonciere {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Sprint 1");
-        System.out.println("Alex branch");
+    public static void main(String[] args) throws IOException {
+        try {
+            String jsonString = FileReader.loadFileIntoString(args[0], "UTF-8");
+            JSONObject fichierEntrer = JSONObject.fromObject(jsonString);
+            JSONObject fichierSortie = new JSONObject();
+
+            fichierSortie.accumulate("type_terrain", fichierEntrer.getString("type_terrain"));
+            fichierSortie.accumulate("prix_m2_min", fichierEntrer.getString("prix_m2_min"));
+            fichierSortie.accumulate("prix_m2_max", fichierEntrer.getString("prix_m2_max"));
+            
+            FileWriter.saveStringIntoFile(args[1], fichierSortie.toString());
+        } catch (IOException e) {
+            System.out.println("Fichier json d'entree introuvable");
+        }
+
+        
     }
-    
+
 }
